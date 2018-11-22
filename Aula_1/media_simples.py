7cc9c152bbe554b761793a3898e2f41b30612ef2
Cícero Henrique <cicerooficial@gmail.com>
2018-11-22 14:49:32 -0200

    correção

--- Aula_1/media_simples.py
+++ Aula_1/media_simples.py
@@ -2,7 +2,7 @@ nomealuno = str(input("Digite o nome do aluno: "))
 numero1 = float(input("Digite a nota do primeiro bimestre do aluno: "))
 numero2 = float(input("Digite a nota do segundo bimestre do aluno: "))
 numero3 = float(input("Digite a nota do terceiro bimestredo aluno: "))
-numero4 = float(input("Digite a nota do quartop bimestredo aluno: "))
+numero4 = float(input("Digite a nota do quarto bimestredo aluno: "))
 
 media = float((numero1+numero2+numero3+numero4)//4)
 
